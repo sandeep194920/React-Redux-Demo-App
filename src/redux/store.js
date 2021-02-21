1f44074f6e1960ec91afa50d3356b56ff0eed7ca
sandeep194920 <sandeepamaranath@gmail.com>
2021-02-20 23:08:08 -0500

    redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer'
 import logger from 'redux-logger'
 
 // adding this logger to the middleware logs onto the browser's console
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
