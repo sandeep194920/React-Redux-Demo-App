03c9be3095ce50c4b817632e777bf5b841a7d6fc
sandeep194920 <sandeepamaranath@gmail.com>
2021-02-21 13:01:13 -0500

    mapDispatchToProps 2nd arg ownProps - useful for dispatching action based on prop passed

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { buyCake, buyIceCream } from '../redux'; //action creator used to dispatch the action
+
+
 // In this component, we need to display either the cake or the icecreams based on the props passed from the parent container
 // So this container can be reused for both cakes and icecreams
 // NOTE : You can implement this in your projects when you click on an item and display it's details. This can be used as the common container for the details 
@@ -8,6 +11,7 @@ function ItemContainer(props) {
     return (
         <div>
             <h2>Item - {props.item} </h2>
+            <button onClick={props.buyItem}>Buy Item</button>
         </div>
     )
 }
@@ -25,4 +29,11 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-export default connect(mapStateToProps)(ItemContainer) 
+const mapDispatchToProps = (dispatch, ownProps) => {
+    const dispatchFunction = ownProps.cake ? () => dispatch(buyCake()) : () => dispatch(buyIceCream())
+    return {
+        buyItem: dispatchFunction
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer) 
